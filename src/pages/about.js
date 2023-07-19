8dbb8ed747cf5744b0d796b1b4944b817a54e6a3
vachbaloyan <vachbaloyan@gmail.com>
2023-07-19 12:07:32 +0400

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const AboutPage = () => {
+    return(
+        <main>
+            <h1>About Us Page</h1>
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </main>
+    )
+}
+export default AboutPage
